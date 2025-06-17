c0bf1fb4661afe5b9ba898721413fc36419aba63
allure <1550220889@qq.com>
2025-06-17 08:36:30 +0800

    feature: 第三方服务调用治理

---
+++ internal/service/sms/async/service.go
@@ -0,0 +1,25 @@
+package async
+
+//import (
+//	"context"
+//	"webook/internal/service/sms"
+//)
+//
+//type SMSService struct {
+//	svc sms.Service
+//}
+//
+//func NewSMSService() *SMSService {
+//	return &SMSService{}
+//}
+//
+//func (s *SMSService) Send(ctx context.Context, biz string, args []string, numbers ...string) error {
+//	// 首先是正常路径
+//	// 异常路径
+//	err := s.svc.Send(ctx, biz, args, numbers...)
+//	if err != nil {
+//		// 判定是不是崩溃了
+//
+//	}
+//}
+//
