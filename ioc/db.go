26b6cddae1f9170cb0114d817afb0661fad9f3d8
allure <1550220889@qq.com>
2025-07-09 08:35:57 +0800

    feature: 日志打印完成

--- ioc/db.go
+++ ioc/db.go
@@ -4,10 +4,13 @@ import (
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	glogger "gorm.io/gorm/logger"
+	"time"
 	"webook/internal/repository/dao"
+	"webook/pkg/logger"
 )
 
-func InitDB() *gorm.DB {
+func InitDB(l logger.LoggerV1) *gorm.DB {
 	type Config struct {
 		DSN string `yaml:"dsn"`
 	}
@@ -16,7 +19,18 @@ func InitDB() *gorm.DB {
 	if err != nil {
 		panic(err)
 	}
-	db, err := gorm.Open(mysql.Open(cfg.DSN))
+	db, err := gorm.Open(mysql.Open(cfg.DSN), &gorm.Config{
+		// 缺了一个 writer
+		Logger: glogger.New(gormLoggerFunc(l.Debug), glogger.Config{
+			// 慢查询阈值，只有执行事件超过这个阈值，才会使用
+			// 50 ms, 100 ms
+			// SQL 查询必然要求命中索引，最好就是走一次磁盘 ID
+			// 一次磁盘 ID 是不到 10 ms
+			SlowThreshold:             time.Millisecond * 10,
+			IgnoreRecordNotFoundError: true,
+			LogLevel:                  glogger.Info,
+		}),
+	})
 	if err != nil {
 		// 只会在初始化过程中 panic
 		// panic 相当于整个 goroutine 结束
@@ -30,4 +44,10 @@ func InitDB() *gorm.DB {
 	}
 
 	return db
+}
+
+type gormLoggerFunc func(msg string, fields ...logger.Field)
+
+func (g gormLoggerFunc) Printf(msg string, args ...interface{}) {
+	g(msg, logger.Field{Key: "args", Value: args})
 }
